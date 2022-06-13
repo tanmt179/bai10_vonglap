caa4e2016131a41d9addb7c9d32c2d9d1eb3e580
tanmt179 <106754526+tanmt179@users.noreply.github.com>
2022-06-13 09:48:23 +0700

    Add files via upload

---
+++ thuchanhfor.js
@@ -0,0 +1,8 @@
+// for(let i=0;i<5;i++){
+//     document.write("The number is "+i+"<br>")
+// }
+// let a=prompt('Nhập giá trị');
+// let sum=0;
+// for(let i=1;i<=a;i++){
+//     sum=sum+i;
+// }document.write('tổng giá trị là:  '+sum)
