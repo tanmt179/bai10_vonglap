3523c7e7f05a9f7f9cf16cc925725a3afb26381a
tanmt179 <106754526+tanmt179@users.noreply.github.com>
2022-06-13 10:06:11 +0700

    Add files via upload

---
+++ thuchanhwhile.js
@@ -0,0 +1,13 @@
+// let num = prompt('Enter a number')
+//  let sum = 0;
+//  while (num !== -1) {
+//      sum = sum + num;
+//      alert(num)
+//      alert(sum)
+//      num = prompt('Enter a number')
+//  }
+// let i=1;
+// while(i<=100){
+//     document.write("<hr width="+i+"%>");
+//     i++;
+// }
